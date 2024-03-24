f9cb02ffb3ce26b63ce70d2c6610a724ad05d276
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-03-24 12:48:56 -0300

    'Classe que realiza as operações de uma tarefa no banc de dados'

---
+++ src/main/java/br/com/jeli/todolist/model/tarefa/TarefaDAO.java
@@ -0,0 +1,19 @@
+package br.com.jeli.todolist.model.tarefa;
+
+
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class TarefaDAO {
+    private EntityManager em;
+
+    public TarefaDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<Tarefa> listarTarefa() {
+        String jpql = "SELECT t FROM Tarefa t";
+        return this.em.createQuery(jpql, Tarefa.class).getResultList();
+    }
+}

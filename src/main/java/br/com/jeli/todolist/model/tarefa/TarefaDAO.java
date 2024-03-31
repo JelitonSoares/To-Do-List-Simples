c7cfed37a20914dc6ce19dabf50e6e89bab632de
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-03-31 11:14:24 -0300

    'Implementando as funcionalidades de atualizar, marcar, cadastrar e remover uma tarefa'

--- src/main/java/br/com/jeli/todolist/model/tarefa/TarefaDAO.java
+++ src/main/java/br/com/jeli/todolist/model/tarefa/TarefaDAO.java
@@ -2,6 +2,8 @@ package br.com.jeli.todolist.model.tarefa;
 
 
 
+import br.com.jeli.todolist.model.urgencia.NivelUrgencia;
+
 import javax.persistence.EntityManager;
 import java.util.List;
 
@@ -15,6 +17,38 @@ public class TarefaDAO {
     public List<Tarefa> listarTarefa() {
         String jpql = "SELECT t FROM Tarefa t";
         return this.em.createQuery(jpql, Tarefa.class).getResultList();
+    }
+
+    public void marcarTarefa(Long id) {
+        this.em.getTransaction().begin();
+        Tarefa tarefa = buscarPorId(id);
+        tarefa.marcarTarefa();
+        this.em.getTransaction().commit();
+    }
+
+    public void atualizarNome(Long id, String nome) {
+        this.em.getTransaction().begin();
+        Tarefa tarefa = buscarPorId(id);
+        tarefa.setTarefa(nome);
+        this.em.getTransaction().commit();
+    }
+
+    public void atualizarUrgencia(Long id, NivelUrgencia urgencia) {
+        this.em.getTransaction().begin();
+        Tarefa tarefa = buscarPorId(id);
+        tarefa.setNivelUrgencia(urgencia);
+        this.em.getTransaction().commit();
+    }
+
+    public void remover(Long id) {
+        this.em.getTransaction().begin();
+        Tarefa tarefa = buscarPorId(id);
+        this.em.remove(tarefa);
+        this.em.getTransaction().commit();
+    }
+
+    private Tarefa buscarPorId(Long id) {
+        return this.em.find(Tarefa.class, id);
     }
 
 

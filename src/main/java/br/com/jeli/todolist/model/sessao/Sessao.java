10944db9a56be42c41c6c6bdd41b5a426b8f4489
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-03-31 09:24:49 -0300

    'Implementando novas funcionalidades'

--- src/main/java/br/com/jeli/todolist/model/sessao/Sessao.java
+++ src/main/java/br/com/jeli/todolist/model/sessao/Sessao.java
@@ -5,12 +5,18 @@ import br.com.jeli.todolist.model.usuario.Usuario;
 public class Sessao {
 
     private String nomeSessao;
+    private Usuario usuario;
 
     public Sessao(Usuario usuario) {
         this.nomeSessao = usuario.getNome();
+        this.usuario = usuario;
     }
 
     public String getNomeSessao() {
         return this.nomeSessao;
+    }
+
+    public Usuario getUsuario() {
+        return this.usuario;
     }
 }

b8d84d787ad0a484036b13403211b23301a8a189
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-03-31 09:25:43 -0300

    'Implementando o metodo para salvar uma tarefa'

--- src/main/java/br/com/jeli/todolist/model/tarefa/TarefaDAO.java
+++ src/main/java/br/com/jeli/todolist/model/tarefa/TarefaDAO.java
@@ -16,4 +16,9 @@ public class TarefaDAO {
         String jpql = "SELECT t FROM Tarefa t";
         return this.em.createQuery(jpql, Tarefa.class).getResultList();
     }
+
+
+    public void cadastrar(Tarefa tarefa) {
+        this.em.persist(tarefa);
+    }
 }
